ddc35760b42f64cea0c7a7a9d1215be37126b29e
wiotrek <piotrwaszak97@gmail.com>
2024-10-27 14:28:21 +0100

    added authview

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/auth',
+      name: 'auth',
+      component: () => import('../views/AuthView.vue')
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
---
+++ frontend/src/views/AuthView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="auth">
+
+    <div class="auth__top">
+      <h3 class="auth__top__h3">Logowanie</h3>
+    </div>
+
+    <div class="auth__center">
+
+      <div class="auth__center__row">
+
+        <div class="auth__center__row__item">
+
+          <label
+              for="username"
+              class="auth__center__row__item__label"
+          >Username</label>
+
+          <input
+              type="text"
+              id="username"
+              class="auth__center__row__item__input"
+          >
+
+        </div>
+
+      </div>
+
+      <div class="auth__center__row">
+
+        <div class="auth__center__row__item">
+
+          <label
+              for="password"
+              class="auth__center__row__item__label"
+          >Username</label>
+
+          <input
+              type="text"
+              id="password"
+              class="auth__center__row__item__input"
+          >
+
+        </div>
+
+      </div>
+
+      <div class="auth__center__row">
+
+        <button class="auth__center__row__submit-btn">
+          Zaloguj sie
+        </button>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .auth {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+</script>
