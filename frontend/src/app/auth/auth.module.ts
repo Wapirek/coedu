a3117cdbc983fd3d20c5a198367c57c62dd66075
wiotrek <piotrwaszak97@gmail.com>
2024-12-08 21:35:58 +0100

    #15 refactored spaces in module

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { RouterModule } from "@angular/router";
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { AuthFormComponent } from "./auth-form/auth-form.component";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from "@angular/material/snack-bar";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
 	declarations: [
@@ -13,29 +13,30 @@ import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from "@angular/materia
 		AuthFormComponent,
 		SignInComponent
 	],
-    imports: [
-        BrowserModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: AuthComponent,
-                children: [
-                    {path: '', component: SignInComponent}
-                ]
-            },
-        ]),
-        ReactiveFormsModule,
-        MatSnackBarModule,
-    ],
-    providers: [
-        {
-            provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
-            useValue: {
-                duration: 2500,
-                horizontalPosition: 'left',
-                verticalPosition: 'bottom'
-            }
-        },
-    ]
+	imports: [
+		BrowserModule,
+		RouterModule.forChild([
+			{
+				path: '',
+				component: AuthComponent,
+				children: [
+					{ path: '', component: SignInComponent }
+				]
+			},
+		]),
+		ReactiveFormsModule,
+		MatSnackBarModule,
+	],
+	providers: [
+		{
+			provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+			useValue: {
+				duration: 2500,
+				horizontalPosition: 'left',
+				verticalPosition: 'bottom'
+			}
+		},
+	]
 })
-export class AuthModule {}
+export class AuthModule {
+}
