8b8ba33b30a944b0e6429475c1d1035e0cdbc1b9
wiotrek <piotrwaszak97@gmail.com>
2024-12-08 22:46:45 +0100

    #15 dodany angular material

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { SitesComponent } from "./sites/sites.components";
+import { AuthGuard } from "./auth/_guards/auth.guard";
 
 export const routes: Routes = [
 	{
 		path: '',
-		component: SitesComponent
+		component: SitesComponent,
+		canActivate: [AuthGuard]
 	},
 	{
 		path: 'auth', loadChildren: () =>
--- frontend/src/app/sites/sites.components.ts
+++ frontend/src/app/sites/sites.components.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: "app-sites",
-	template: '<router-outlet></router-outlet>',
+	template: '<router-outlet />',
 	standalone: false
 })
 export class SitesComponent {}
